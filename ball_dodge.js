9d6c0aaf20ace838cf8db6e536a34c5b0ad190fc
jhazlitt <bagelday@gmail.com>
2015-09-22 12:45:17 -0600

    Changed the menu and the way the score is displayed

--- ball_dodge.js
+++ ball_dodge.js
@@ -1,106 +1,87 @@
 $(document).ready(function() {
 	var left = 200;
 	var top = 200;
-	$('#circle').html(left + "," + top);
+	var score = 0;
 
-	$('#title_window').click(playGame);
-	function playGame() {
+	var enemy1 = {
+		name: "enemy1",
+		left: 0,
+		top: 0
+	};
+	
+	var enemy2 = {
+		name: "enemy2",
+		left: 0,
+		top: 400		
+	};
+
+	var enemy3 = {
+		name: "enemy3",
+		left: 400,
+		top: 0
+	};
+
+	var enemy4 = {
+		name: "enemy4",
+		left: 400,
+		top: 400
+	};
+
+	$('#title_window').click(setupGame);
+
+	function setupGame() {
 		$('#title_window').hide();
+		$('#end_window').hide();
 		$('#game').append('<div id="circle"></div>');
 		$('#game').append('<div id="enemy1"></div>');
 		$('#game').append('<div id="enemy2"></div>');
 		$('#game').append('<div id="enemy3"></div>');
 		$('#game').append('<div id="enemy4"></div>');
-		// score will increase by one for each second over the start time
+		playGame();
+	}
 
+	function playGame() {
 		$(document).keydown(function(key) {
 			switch(parseInt(key.which,10)) {
 				// Left arrow key pressed
 				case 37:
+					collisionDetect();
 					if (left >= 100){
 						$('#circle').animate({left: "-=100px"}, 'fast');				
 						left -= 100;
-						$('#circle').html(left + "," + top);
 					}
 					break;
 				// Up arrow pressed
 				case 38:
+					collisionDetect();
 					if (top >= 100) {
 						$('#circle').animate({top: "-=100px"}, 'fast');				
 						top -= 100;
-						$('#circle').html(left + "," + top);
 					}
 					break;
 				// Right arrow pressed
 				case 39:
+					collisionDetect();
 					if (left <= 300) {
 						$('#circle').animate({left: "+=100px"}, 'fast');				
 						left += 100;
-						$('#circle').html(left + "," + top);
 					}
 					break;
 				// Down arrow pressed 
 				case 40:
+					collisionDetect();
 					if (top <= 300) {
 						$('#circle').animate({top: "+=100px"}, 'fast');				
 						top += 100;
-						$('#circle').html(left + "," + top);
 					}
 					break;
 			}
 		});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 		var start = new Date;
-		var score = 0;
-
-		var enemy1 = {
-			name: "enemy1",
-			left: 0,
-			top: 0
-		};
-		
-		var enemy2 = {
-			name: "enemy2",
-			left: 0,
-			top: 400		
-		};
-
-		var enemy3 = {
-			name: "enemy3",
-			left: 400,
-			top: 0
-		};
-
-		var enemy4 = {
-			name: "enemy4",
-			left: 400,
-			top: 400
-		};
 
 		setInterval(function() {
+			collisionDetect();
 			$('#timer').text("Score: " + score);
 			score = Math.round((new Date- start) / 1000);
 			
@@ -109,6 +90,21 @@ $(document).ready(function() {
 			enemyMove(enemy3);
 			enemyMove(enemy4);
 		}, 1000);
+	}
+
+	function collisionDetect() {
+		if ((left === enemy1.left) && (top === enemy1.top)) {
+			gameOver();
+		} 
+		else if ((left === enemy2.left) && (top === enemy2.top)) { 
+			gameOver();
+		}
+		else if ((left === enemy3.left) && (top === enemy3.top)) {
+			gameOver();
+		} 
+		else if ((left === enemy4.left) && (top === enemy4.top)) {
+			gameOver();
+		}
 	}
 
 	function enemyMove(enemy) {
@@ -171,5 +167,17 @@ $(document).ready(function() {
 		}
 	}
 
+	function gameOver() {
+		$('#timer').hide();
+		$('#circle').hide();
+		$('#enemy1').hide();
+		$('#enemy2').hide();
+		$('#enemy3').hide();
+		$('#enemy4').hide();
 
+		$('#game').append('<div id="end_window"><h1>Game Over!</h1><h2>Your score: ' + score + '</h2><h2>Please refresh the page to play again.</h2></div>');
+		// Set left and top to arbitrary numbers so that the gameOver function ceases to be called
+		left = 123;
+		top = 123;
+	}
 });
